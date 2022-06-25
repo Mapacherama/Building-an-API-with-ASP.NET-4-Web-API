621143d484206f6ba03747bcd973a7c34c4015f1
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-25 09:16:55 +0200

    Starting this new course

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+
+        private readonly ICampRepository _campRepository;
+        public CampsController(ICampRepository campRepository)
+        {
+            _campRepository = campRepository;
+        }
+        public async Task<IHttpActionResult> Get()
+        {
+            var result = await _campRepository.GetAllCampsAsync();
+            
+            return Ok(result);
+        }
+    }
+}
+
