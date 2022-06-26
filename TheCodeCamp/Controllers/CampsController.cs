91bcd0a49f57833406abf0611b29abd90547e4df
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-26 06:44:46 +0200

    Almost done with Module 3.

--- TheCodeCamp/Controllers/CampsController.cs
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -1,22 +1,58 @@
-﻿using System.Threading.Tasks;
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Web.Http;
 using TheCodeCamp.Data;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
 
-        private readonly ICampRepository _campRepository;
-        public CampsController(ICampRepository campRepository)
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
-            _campRepository = campRepository;
+            _repository = repository;
+            _mapper = mapper;
         }
+        [Route()]
         public async Task<IHttpActionResult> Get()
         {
-            var result = await _campRepository.GetAllCampsAsync();
-            
-            return Ok(result);
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+
+                // Mapping 
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+
+                return Ok(mappedResult);
+            }
+            catch (Exception ex)
+            {
+                // TODO Add Logging
+                return InternalServerError(ex);
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if(result == null) return NotFound();
+                
+                return Ok(_mapper.Map<CampModel>(result));
+            } 
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
     }
 }
