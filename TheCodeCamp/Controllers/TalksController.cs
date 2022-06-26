ea2cdf3016511401dec91ecd846a8f466ded3a6b
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-26 13:06:56 +0200

    Module 6 is done.

--- TheCodeCamp/Controllers/TalksController.cs
+++ TheCodeCamp/Controllers/TalksController.cs
@@ -37,7 +37,7 @@ namespace TheCodeCamp.Controllers
             }
         }
 
-        [Route("{id:int}")]
+        [Route("{id:int}", Name = "GetTalk")]
         public async Task<IHttpActionResult> Get(string moniker, int id, bool includeSpeakers = false)
         {
             try
@@ -52,6 +52,99 @@ namespace TheCodeCamp.Controllers
             }
         }
 
+        [Route()]
+        public async Task<IHttpActionResult> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var camp = await _repository.GetCampAsync(moniker);
+                    if (camp != null)
+                    {
+                        var talk = _mapper.Map<Talk>(model);
+                        talk.Camp = camp;
+                        // Map the Speaker if necessary
+                        if (model.Speaker != null)
+                        {
+                            var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                            if (speaker != null) talk.Speaker = speaker;
+                        }
+                        _repository.AddTalk(talk);
+
+                        if(await _repository.SaveChangesAsync())
+                        {
+                            return CreatedAtRoute("GetTalk", new { moniker = moniker, id = talk.TalkId },
+                                _mapper.Map<TalkModel>(talk));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [Route("{talkId:int}")]
+        public async Task<IHttpActionResult> Put(string moniker, int talkId, TalkModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var talk = await _repository.GetTalkByMonikerAsync(moniker, talkId, true);
+                    if (talk == null) return NotFound();
+                    // It's going to ignore the Speaker
+                    _mapper.Map(model, talk);
+
+                    // Change speaker if needed
+                    if (talk.Speaker.SpeakerId != model.Speaker.SpeakerId)
+                    {
+                        var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                        if (speaker != null) talk.Speaker = speaker;
+                    }
+
+                    if (await _repository.SaveChangesAsync())
+                    {
+                        Ok(_mapper.Map<TalkModel>(talk));
+                    }
+                }             
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [Route("{talkId:int}")]
+        public async Task<IHttpActionResult> Delete(string moniker, int talkId)
+        {
+            try
+            {
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, talkId);
+                if (talk == null) return NotFound();
+
+                _repository.DeleteTalk(talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return InternalServerError();
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
 
     }
 }
--- TheCodeCamp/Data/CampMappingProfile.cs
+++ TheCodeCamp/Data/CampMappingProfile.cs
@@ -13,8 +13,10 @@ namespace TheCodeCamp.Data
         public CampMappingProfile()
         {
             CreateMap<Camp, CampModel>().ForMember(c => c.Venue, opt => opt.MapFrom(m => m.Location.VenueName)).ReverseMap();
-            CreateMap<Talk, TalkModel>();
-            CreateMap<Speaker, SpeakerModel>();
+            CreateMap<Talk, TalkModel>().ReverseMap()
+                .ForMember(t => t.Speaker, opt => opt.Ignore())
+                .ForMember(t => t.Camp, opt => opt.Ignore());
+            CreateMap<Speaker, SpeakerModel>().ReverseMap();
         }
     }
 }
